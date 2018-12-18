b870491b4b0ec1c01a60ea7883c9a0dd5e233cfd
Johannes Hermjohannknecht <j.hermjohannknecht@tum.de>
2018-12-18 18:03:06 +0100

    implementation applied_clearing_function

--- src/Auction.py
+++ src/Auction.py
@@ -61,3 +61,56 @@ def uniform_distribution(min, max):
         min + sp.stats.uniform() * (max - min)
     return dist
 
+def applied_clearing_function(bids, courses):
+
+    n_player = len(bids)
+    n_courses = len(courses)
+    price=[0]*n_courses
+    left_capacity=[0]*n_courses
+    assignment = [-1, -1] * n_player     #1st position: assigned class, 2nd cost of class
+
+    for c in range(n_courses):
+        left_capacity[c] = courses[c].capacity
+
+    for i in range(n_player):
+        # search for highest bid
+        highest_bid_player = -1
+        highest_bid_course = -1
+        highest_bid_value = -1
+
+        for p in range(n_player):
+            if assignment[p][0] is not -1:
+                p = p+1
+            else:
+                for c in range(n_courses):
+                    if left_capacity[c] < 0:
+                        c = c+1
+                    else:
+                        if bids[p][c] > highest_bid_value:
+                            highest_bid_value = bids[p][c]
+                            highest_bid_player = p
+                            highest_bid_course = c
+
+
+        if left_capacity[highest_bid_course] is 0:          # set price if first player cant enter
+            price[highest_bid_course] = highest_bid_value
+            left_capacity[highest_bid_course] -= 1
+        else:                                               # assign player if free spot avaliable
+            assignment[highest_bid_player][0] = highest_bid_course
+            left_capacity[highest_bid_course] -= 1
+
+    for i in range(n_player):
+        assignment[i][1] = price[assignment[i][0]]
+
+    return assignment
+
+    # assign player corresponding to highest bid
+    # if player can be assigned: remove all bids of player
+    # else: update price (price array)of course and remove all bids for that course
+
+
+
+
+
+    # assign bid to course
+
