8d0ae2b22556262886bb51844acb2de64c91a86d
hjorthjort <rikard.hjort@gmail.com>
2018-12-18 18:00:55 +0100

    Make runnable auctions, add simple test

---
+++ test/TestAuction.py
@@ -0,0 +1,13 @@
+import unittest
+from src.Auction import *
+
+
+class TestAuction(unittest.TestCase):
+
+    def test_run_simple(self):
+        auction = Auction()  # Default implementation.
+        res = auction.run_auction()  # Just make sure nothing fails.
+        print(res)  # TODO: Remove
+
+if __name__ == '__main__':
+    unittest.main()
