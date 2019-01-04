8f0f2ff8cba951816a306acbddf6a83c40751619
Emanuel <trahald@users.noreply.github.com>
2019-01-04 17:04:04 +0100

    I've added some code to simulate several auctions and count the number of wins of each strategy. The players select the strategies at random. I am aware that the clearing function has bugs and all the other things.

--- src/Auction.py
+++ src/Auction.py
@@ -44,7 +44,7 @@ class Player:
 
     def __init__(self, strategy=None):
         if strategy is None:
-            self.strategy = _default_strategy
+            self.strategy = default_strategy
         else:
             self.strategy = strategy
 
@@ -98,7 +98,7 @@ def default_clearing_function(bids: List[Dict[Course, float]]) -> List[Tuple[flo
     return assignments
 
 
-def _default_strategy(max_tokens: float, _num_players: int, utilities: Dict[Course, float]) -> Dict[Course, float]:
+def default_strategy(max_tokens: float, _num_players: int, utilities: Dict[Course, float]) -> Dict[Course, float]:
     """The "all-in" strategy."""
     bids = {}
     max_utility = -math.inf
@@ -195,7 +195,7 @@ def applied_clearing_function(bids, courses):
 #  will update the plot data with new data being inputted.
 #  This function initializes the plot with empty data.
 #  It takes as INPUT the number of strategies you want to plot.
-def init_plot_population(number_of_strat):
+def init_plot_population(number_of_strat, names_of_strats):
     strategy_population = [None] * number_of_strat
     for i in range(0, number_of_strat):
         strategy_population[i] = np.array([])
@@ -223,18 +223,19 @@ def init_plot_population(number_of_strat):
 
     # See
     #  https://matplotlib.org/gallery/style_sheets/style_sheets_reference.html
-    # for different style types.
+    #  for different style types.
     plt.style.use('seaborn-talk')
 
-    # If LaTeX gives you problems disable these labels.
-   # plt.xlabel(r'Iteration number ($t$)')
-   # plt.ylabel(r'Population fraction $x_i$')
-   # plt.title(r'Evolution of population fraction')
+    plt.xlabel('Run number')
+    plt.ylabel('Win number')
+    plt.title('Win count for different strategies after averaging')
 
     plot_list = []
     for i in range(0, number_of_strat):
         line, = plt.plot(strategy_population[i], color=tableau20[i % 20])
         plot_list.append(line)
+
+    plt.legend(plot_list, names_of_strats)
 
     plt.show()
     # plt.savefig("graph.png", bbox_inches="tight")
@@ -288,57 +289,101 @@ for t in time:
     # Update the plot:
     update_plot(plot_output, t, new_data)
 '''
-'''
-# Three players and three classes.
+
+### Control variables
 number_of_courses = 3
+number_of_players = 3
+number_of_measurements = 5      # Between how many runs do we stop to update
+                                # the plot and measure.
+runs_between_measures = 100    # How many times run the whole process, ie,
+                                # how many times to start over with different
+                                # strategies.
+number_to_average = 200     # How much to average before looking at the highest
+                            # utility to determine the winner. I've chosen this
+                            # number as it is such that the variance intervals
+                            # centered on the mean don't overlap between the
+                            # winner and the second in line.
+
+types_of_strategies  = [default_strategy,
+                        random_strategy]
+number_of_strategies = len(types_of_strategies)
+names_of_strategies  = [types_of_strategies[i].__name__
+                        for i in range(0, number_of_strategies)]
+'''
+plot_output = init_plot_population(number_of_strategies,
+                                   names_of_strategies)
+'''
+
+win_counter = np.zeros([number_of_strategies,
+                        number_of_measurements*runs_between_measures])
+
 course_list = [Course(name=str(i), capacity=1)
                for i in range(0, number_of_courses)]
 
-# For the moment we only have one default strategy.
-types_of_strategies = [ _default_strategy,
-                        random_strategy]
-number_of_strategies = len(types_of_strategies)
+for i_measure in range(0, number_of_measurements): # start from 1 to avoid having
+                                                   # to define some if statements
+    for run in range(0, runs_between_measures):
+        player_list = [Player(strategy=random.choice(types_of_strategies))
+                                for i in range(0, number_of_players) ]
 
-number_of_players = 3
-player_list = [Player(strategy=random.choice(types_of_strategies))
-                        for i in range(0, number_of_players) ]
+        utility = np.zeros([number_of_players, number_to_average])
+        average_utility = np.zeros(number_of_players)
+        sigma_utility   = np.zeros(number_of_players) # For std. deviation
 
-number_of_wins = np.zeros(number_of_strategies) # This will serve as a counter.
+        # In this loop we run the auction several times and record final utilities.
+        for i in range(0, number_to_average):
+            auction = Auction(players = player_list, courses = course_list)
+            outcome = auction.run_auction()
+            payments  = outcome[0] # Type List[Dict[Course, float]]
+            utilities = outcome[1] # Type List[Tuple[float, Course]]
+            for pl_i in range(0, number_of_players):
+                course_won      = payments[pl_i][1]
+                utility[pl_i,i] = ( utilities[pl_i][course_won]
+                                   -payments[pl_i][0])
+        # Now we average.
+        for i in range(0,number_of_players):
+            average_utility[i] = np.average(utility[i,:])
+            # Using the formula for the standard deviation of the mean or average:
+            sigma_utility[i]   = (  np.sqrt(np.var(utility[i,:]))
+                                   /np.sqrt(number_to_average)    )
 
-number_of_runs    = 10
-number_to_average = 100
-utility = np.zeros([number_of_players, number_to_average])
-average_utility = np.zeros(number_of_players)
-for i in range(0, number_to_average):
-    auction = Auction(players=player_list, courses=course_list)
-    outcome = auction.run_auction()
-    all_payments  = outcome[0] # Type List[Dict[Course, float]]
-    all_utilities = outcome[1] # Type List[Tuple[float, Course]]
-    for pl_i in range(0, number_of_players):
-        course_won      = all_payments[pl_i][1]
-        utility[pl_i,i] = all_utilities[pl_i][course_won]-all_payments[pl_i][0]
-for i in range(0,number_of_players):
-    average_utility[i] = np.average(utility[i,:])
-# Now we find the winner.
-winner_idx = np.argmax(average_utility)
-print(player_list[winner_idx].strategy.__name__)
+        # Now we find the winner.
+        winner_idx = np.argmax(average_utility)
+        # The following gets the name of the winning strategy and finds its index.
+        winning_strategy_idx = names_of_strategies.index(
+                                    player_list[winner_idx].strategy.__name__ )
 
-'''
+        aux_idx = i_measure*runs_between_measures + run
+        win_counter[:,aux_idx] = win_counter[:,aux_idx-1]
+        win_counter[winning_strategy_idx,aux_idx] += 1
 
-'''
-time = np.arange(0.1, 20, 0.1)
-plot_output = init_plot_population(3)  # plot_output[0] is the figure
-# plot_output[1] is a list of line
-# objects from which to pull the
-# x and y data from
-new_data = [None] * 3
-for t in time:
-    # Generate points to add to the plot:
-    new_data[0] = np.exp(-t)
-    new_data[1] = np.sin(t)
-    new_data[2] = np.log(t)
 
-    # Update the plot:
-    update_plot(plot_output, t, new_data)
-'''
+    print('Measurement = '+str(i_measure)+'. Average utilities:')
+    for i in range(0, number_of_players):
+        print('Player',i,
+              '{0: <16}'.format(player_list[i].strategy.__name__), '\t',
+              round(average_utility[i],1), "+-",
+              round(sigma_utility[i]  ,1))
+    print('\n')
+
+    #update_plot(plot_output, i_measure, win_counter[i_measure])
+
+# The winning strategy is the one with most win counts at the wnd:
+final_winning_strategy_idx = np.argmax(win_counter[:,-1])
+print('*******')
+print('And the winner iiiissss .....: ',
+            names_of_strategies[final_winning_strategy_idx])
+print('*******')
+
+x_axis = np.arange(0, number_of_measurements*runs_between_measures, 1)
+for i in range(0, number_of_strategies):
+    plt.plot(x_axis, win_counter[i,:], label=names_of_strategies[i])
+
+# If the plot functions don't work use this, which plots at the end. With the
+# plot functions you plot live.
+plt.xlabel('Run number')
+plt.ylabel('Win count')
+plt.title('Win count for different strategies after averaging')
+plt.legend()
+plt.show()
 
